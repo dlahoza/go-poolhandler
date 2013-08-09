5c4686eaa6862e7f7aa83941befe465dde2b8eff
Dmitry Lagoza <dmitry@lagoza.name>
2013-08-09 09:51:19 +0300

    Rename, fixes, cleanup

--- go-poolhandler.go
+++ go-poolhandler.go
@@ -1,28 +1,27 @@
-package mpmhandler
+package poolhandler
 
 import (
+	"io"
 	"net/http"
 	"strconv"
-	"io"
 	"time"
-	"fmt"
 )
 
-type mpmHandler struct {
-handler http.Handler
-reqchan chan poolRequest
+type PoolHandler struct {
+	handler http.Handler
+	reqchan chan poolRequest
 }
 
 type poolRequest struct {
-	w http.ResponseWriter
-	req *http.Request
+	w       http.ResponseWriter
+	req     *http.Request
 	handler http.Handler
-	result chan bool
+	result  chan bool
 }
 
 type poolWorker struct {
-	Busy bool
-	Start, End time.Time
+	Busy                           bool
+	Start, End                     time.Time
 	Method, Host, Path, RemoteAddr string
 }
 
@@ -41,21 +40,18 @@ func poolManager(maxclients int, reqchan chan poolRequest) {
 	var poolreq poolRequest
 	var totalCount int
 	var nowCount int
-	free:=make(chan bool,2)
-	pool:=make([]poolWorker,maxclients,maxclients)
+	free := make(chan bool, 2)
+	pool := make([]poolWorker, maxclients, maxclients)
 	for {
 		if nowCount < maxclients {
-			//fmt.Println("Wait for req")
 			select {
 			case poolreq = <-reqchan:
-				//fmt.Println("Get req")
 				totalCount++
 				nowCount++
-				for key:= range pool {
+				for key := range pool {
 					if !pool[key].Busy {
 						pool[key].Busy = true
-						//fmt.Println("Start work")
-						if (poolreq.req.URL.Path=="/pool-status") {
+						if poolreq.req.URL.Path == "/pool-status" {
 							pool[key].Method = poolreq.req.Method
 							pool[key].Host = poolreq.req.Host
 							pool[key].Path = poolreq.req.URL.Path
@@ -63,7 +59,7 @@ func poolManager(maxclients int, reqchan chan poolRequest) {
 							io.WriteString(poolreq.w, "<html><head></head><body>")
 							io.WriteString(poolreq.w, "Total requests: "+strconv.Itoa(totalCount)+"<br />")
 							io.WriteString(poolreq.w, "Active requests: "+strconv.Itoa(nowCount)+"/"+strconv.Itoa(maxclients)+"<br />")
-							for _,worker:= range pool {
+							for _, worker := range pool {
 								if worker.Busy {
 									io.WriteString(poolreq.w, "W")
 								} else {
@@ -76,7 +72,7 @@ func poolManager(maxclients int, reqchan chan poolRequest) {
 							io.WriteString(poolreq.w, "<td><b>Client</b></td>")
 							io.WriteString(poolreq.w, "<td><b>VHost</b></td>")
 							io.WriteString(poolreq.w, "<td nowrap><b>Request</b></td></tr>")
-							for k,worker:= range pool {
+							for k, worker := range pool {
 								io.WriteString(poolreq.w, "<tr><td><b>"+strconv.Itoa(k)+"</b></td>")
 								if worker.Busy {
 									io.WriteString(poolreq.w, "<td><b>W</b></td>")
@@ -89,11 +85,9 @@ func poolManager(maxclients int, reqchan chan poolRequest) {
 								io.WriteString(poolreq.w, "</tr>")
 							}
 							io.WriteString(poolreq.w, "</table></body></html>\n")
-							fmt.Println("End work")
 							pool[key].Busy = false
 							nowCount--
 							poolreq.result <- true
-							fmt.Println("End")
 						} else {
 							go pool[key].DoWork(poolreq, free)
 						}
@@ -101,27 +95,23 @@ func poolManager(maxclients int, reqchan chan poolRequest) {
 					}
 				}
 			case <-free:
-				//fmt.Println("End work")
 				nowCount--
 			}
 		} else {
 			<-free
-			//fmt.Println("End waited work")
 			nowCount--
-		}			
+		}
 	}
 }
 
-func (h mpmHandler) ServeHTTP(w http.ResponseWriter, req *http.Request) {
-	//count++
-	//io.WriteString(w, strconv.Itoa(count)+"\n")
-	result:=make(chan bool)
-	h.reqchan <- poolRequest{w,req,h.handler,result}
-	<- result
+func (h PoolHandler) ServeHTTP(w http.ResponseWriter, req *http.Request) {
+	result := make(chan bool)
+	h.reqchan <- poolRequest{w, req, h.handler, result}
+	<-result
 }
 
-func MpmHandler(maxclients int, h http.Handler) http.Handler {
-	reqchan:=make(chan poolRequest)
+func GetPoolHandler(maxclients int, h http.Handler) http.Handler {
+	reqchan := make(chan poolRequest)
 	go poolManager(maxclients, reqchan)
-	return mpmHandler{h,reqchan}
+	return PoolHandler{h, reqchan}
 }
